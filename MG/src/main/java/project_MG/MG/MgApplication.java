8454cf60daffe920c6f9428f548802fff307b5e6
asdnng <asdnng@gmail.com>
2025-08-21 23:14:47 +0200

    Initial commit

---
+++ MG/src/main/java/project_MG/MG/MgApplication.java
@@ -0,0 +1,13 @@
+package project_MG.MG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgApplication.class, args);
+	}
+
+}
---
+++ MG/src/test/java/project_MG/MG/MgApplicationTests.java
@@ -0,0 +1,13 @@
+package project_MG.MG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
