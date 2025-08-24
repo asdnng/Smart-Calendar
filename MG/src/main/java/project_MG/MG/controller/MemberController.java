d3474b133d0975862613c9eacf23321345499afb
asdnng <asdnng@gmail.com>
2025-08-25 01:00:07 +0200

    add member form. need modification

---
+++ MG/src/main/java/project_MG/MG/controller/MemberController.java
@@ -0,0 +1,19 @@
+package project_MG.MG.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import project_MG.MG.service.MemberService;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+}
