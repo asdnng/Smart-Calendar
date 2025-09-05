3fb9007031b64576f3354cd84e320a9cfaaab955
asdnng <asdnng@gmail.com>
2025-09-05 19:09:14 +0200

    mock api test

--- MG/src/main/frontend/src/App.js
+++ MG/src/main/frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+//
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
+
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [oauthUrl, setOauthUrl] = useState('');
+
+    const fetchOAuthUrl = () => {
+        const redirectUri = encodeURIComponent('http://localhost:3000/callback');
+        axios.get(`/api/oauth/login/google?redirectUri=${redirectUri}`)
+            .then(response => setOauthUrl(response.data))
+            .catch(error => console.error('Error fetching OAuth URL:', error));
+    };
+
+    return (
+        <div>
+            <button onClick={fetchOAuthUrl}>Get Google OAuth URL</button>
+            {oauthUrl && (
+                <div>
+                    <p>Generated OAuth URL:</p>
+                    <a href={oauthUrl} target="_blank" rel="noopener noreferrer">{oauthUrl}</a>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
