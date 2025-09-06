195527d8a46574165b8e3fcbbc561e336f232f50
Mun <113595508+charanan@users.noreply.github.com>
2025-09-06 15:54:18 +0200

    Update body.js

--- MG/src/main/frontend/src/components/body.js
+++ MG/src/main/frontend/src/components/body.js
@@ -1,1 +1,11 @@
+function Body() {
+  return (
+    <div className="row flex-grow-1">
+      <div className="col-2 bg-light d-flex justify-content-center align-items-center p-3">Menu</div>
+      <div className="col-8 bg-dark text-white d-flex justify-content-center align-items-center p-3">Main board</div>
+      <div className="col-2 bg-light d-flex justify-content-center align-items-center p-3">Side tab</div>
+    </div>
+  );
+}
 
+export default Body;
