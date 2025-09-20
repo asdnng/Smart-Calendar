e824295720e45f2bf221925073f29d66fb9eeae0
asdnng <asdnng@gmail.com>
2025-09-20 14:53:01 +0200

    Add join function and api to check specific member

--- MG/src/main/java/project_MG/MG/controller/MemberController.java
+++ MG/src/main/java/project_MG/MG/controller/MemberController.java
@@ -1,11 +1,14 @@
 package project_MG.MG.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import project_MG.MG.domain.Member;
 import project_MG.MG.service.MemberService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,6 +16,22 @@ import java.util.List;
 public class MemberController {
     private final MemberService memberService;
     public MemberController(MemberService memberService) { this.memberService = memberService; }
+
+    @PostMapping
+    public ResponseEntity<Long> join(@RequestBody Member member) {
+        Long id = memberService.join(member);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(id).toUri();
+        return ResponseEntity.created(location).body(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Member> find(@PathVariable Long id) {
+        return memberService.findOne(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     @PostMapping
     public Long joinMember(@RequestBody Member member) {
