f411cfd3af54d5fb23a71aa2ab63c9c7047168a1
asdnng <asdnng@gmail.com>
2025-09-20 14:08:38 +0200

    edit membercontroller

--- MG/src/main/java/project_MG/MG/controller/MemberController.java
+++ MG/src/main/java/project_MG/MG/controller/MemberController.java
@@ -6,18 +6,21 @@ import org.springframework.web.bind.annotation.*;
 import project_MG.MG.domain.Member;
 import project_MG.MG.service.MemberService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    public MemberController(MemberService memberService) { this.memberService = memberService; }
 
-    @PostMapping("/api/members")
-    @ResponseBody
+    @PostMapping
     public Long joinMember(@RequestBody Member member) {
         return memberService.join(member);
     }
 
+    @GetMapping
+    public List<Member> listMembers() {
+        return memberService.findMembers();
+    }
 }
