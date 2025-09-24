a5042d2b9ae42485ce4068a845277ceaa11a3e1f
asdnng <asdnng@gmail.com>
2025-09-24 17:33:13 +0200

    Add Member,Jwt Controller

--- MG/src/main/java/project_MG/MG/controller/HomeController.java
+++ MG/src/main/java/project_MG/MG/controller/HomeController.java
@@ -1,11 +1,11 @@
-package project_MG.MG.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-@Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "forward:/index.html";
-    }
-}
+//package project_MG.MG.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    public String home() {
+//        return "forward:/index.html";
+//    }
+//}
