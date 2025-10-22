5afa49e5ad978ec3366d796349800ac35b968f6d
charanan <charananma@gmail.com>
2025-10-22 15:14:58 +0200

    Fix time logic for UX/UI

--- MG/src/main/frontend/src/components/crud.js
+++ MG/src/main/frontend/src/components/crud.js
@@ -2,18 +2,21 @@ import { useState } from 'react';
 
 import { BsFillTrashFill } from 'react-icons/bs';
 
+import { useTasks } from './Tasks.js';
 import SelectCategory from './categories.js';
 import Alert from './alert.js';
 
 import '../cssModules/crud.css';
 
 function CRUD({ isOpen, onClose }) {
+  const { addTask } = useTasks();
   /* TASK INPUTS VARIABLES */
   const [taskName, setTaskName] = useState("");
   const [category, setCategory] = useState("");
   const [date, setDate] = useState("");
   const [startTime, setStartTime] = useState(""); // user optional
   const [endTime, setEndTime] = useState(""); // user optional
+  const [timeError, setTimeError] = useState("");
   const [description, setDescription] = useState(""); // user optional
   /* ALERT CONFIRMATION VARIABLE */
   const [showConfirm, setShowConfirm] = useState(false);
@@ -24,19 +27,54 @@ function CRUD({ isOpen, onClose }) {
     setDate("");
     setStartTime("");
     setEndTime("");
+    setTimeError("");
     setDescription("");
+
+    onClose();
   };
 
   const handleDeleteClick = () => {
     const hasData = taskName || category || date || startTime || endTime || description;
     if (hasData) { setShowConfirm(true); }
-    else { onClose(); }
+    else { resetForm(); }
   };
 
   const confirmDelete = () => {
-    resetForm();
     setShowConfirm(false);
-    onClose();
+    resetForm();
+  };
+
+  const handleTimeChange = (time, setTime) => {
+    setTime(time);
+    setTimeError("");
+  };
+
+  const checkTime = () => {
+    if (endTime && !startTime) {
+      setTimeError("Start time is required if end time exists");
+      return false;
+    } else if (startTime && endTime && (endTime < startTime)) {
+     setTimeError("Fill earlier start time or later end time");
+     return false;
+    } else return true;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!checkTime()) return;
+
+    const newTask = {
+      id: Date.now(),
+      taskName,
+      category,
+      date,
+      startTime,
+      endTime,
+      description
+    };
+
+    addTask(newTask);
+    resetForm();
   };
 
   if (!isOpen) return null;
@@ -53,7 +91,7 @@ function CRUD({ isOpen, onClose }) {
         </button>
 
         {/* CRUD FORM */}
-        <form className="crud-form mt-4">
+        <form className="crud-form mt-4" onSubmit={handleSubmit}>
           {/* TASK NAME */}
           <input
             type="text"
@@ -87,20 +125,26 @@ function CRUD({ isOpen, onClose }) {
           </div>
 
           {/* TIME */}
-          <div className="d-flex align-items-center">
-            <input
-              type="time"
-              className="form-control"
-              value={startTime}
-              onChange={(e) => setStartTime(e.target.value)}
-            />
-            <span className="mx-2">til</span>
-            <input
-              type="time"
-              className="form-control"
-              value={endTime}
-              onChange={(e) => setEndTime(e.target.value)}
-            />
+          <div>
+            <div className="d-flex align-items-center">
+              <input
+                type="time"
+                className={`form-control ${timeError ? "time-error" : ""}`}
+                value={startTime}
+                onChange={(e) => handleTimeChange(e.target.value, setStartTime)}
+              />
+              <span className="mx-2">til</span>
+              <input
+                type="time"
+                className={`form-control ${timeError ? "time-error" : ""}`}
+                value={endTime}
+                onChange={(e) => handleTimeChange(e.target.value, setEndTime)}
+              />
+            </div>
+
+            {timeError && (
+              <p className="text-danger mt-1 mb-0">{timeError}</p>
+            )}
           </div>
 
           {/* TASK DESCRIPTION */}
