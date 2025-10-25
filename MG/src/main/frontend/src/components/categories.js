6d628e835b08ea005539f7d44db6913251c21b11
charanan <charananma@gmail.com>
2025-10-25 21:55:44 +0200

    Make tag color responsive to crud modes

--- MG/src/main/frontend/src/components/categories.js
+++ MG/src/main/frontend/src/components/categories.js
@@ -1,25 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { BsBookmarkFill } from 'react-icons/bs';
 
-const categories = [
-  { value: "personal", label: "Personal", color: "#597ac9" },
-  { value: "work", label: "Work", color: "#8e6eb5" },
-  { value: "study", label: "Study", color: "#65a16e" },
-  { value: "other", label: "Other", color: "gray" }
+export const Categories = [
+  { value: "personal", label: "Personal", color: "rgba(89, 122, 201, 1)" },
+  { value: "work", label: "Work", color: "rgba(142, 110, 181, 1)" },
+  { value: "study", label: "Study", color: "rgba(101, 161, 110, 1)" },
+  { value: "other", label: "Other", color: "rgba(130, 130, 130, 1)" }
 ];
 
 function SelectCategory({ selected, onSelect }) {
   const [tagColor, setTagColor] = useState("#f3ebeb");
 
+  const handleTagColor = (value) => {
+    const selectedCategory = Categories.find((c) => c.value === value);
+    if (selectedCategory) { setTagColor(selectedCategory.color); }
+    else { setTagColor("white"); }
+  };
+
   const handleChange = (e) => {
     const value = e.target.value;
     onSelect(value);
-
-    const selectedCategory = categories.find((c) => c.value === value);
-    if (selectedCategory) { setTagColor(selectedCategory.color); }
-    else { setTagColor("white"); }
+    handleTagColor(value);
   }
+
+  useEffect(() => {
+    if (selected) handleTagColor(selected);
+  }, []);
 
   return (
     <div className="category-option d-flex align-items-center">
@@ -32,7 +39,7 @@ function SelectCategory({ selected, onSelect }) {
 
       {/* OPTION */}
       <select
-        className="form-select bg-transparent"
+        className="form-select cursor-pointer bg-transparent"
         value={selected}
         onChange={handleChange}
         required
@@ -40,7 +47,7 @@ function SelectCategory({ selected, onSelect }) {
         <option value="" disabled hidden>
           -- Select category --
         </option>
-        {categories.map((c) => (
+        {Categories.map((c) => (
           <option key={c.value} value={c.value}>{c.label}</option>
         ))}
       </select>
