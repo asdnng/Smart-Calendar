a7b8ecf2d606f76c5610bc216b1df1e2a35f4317
asdnng <asdnng@gmail.com>
2025-10-26 15:53:49 +0100

    fix 401 error(Skip Authorization Header for Login/Signup Requests)

--- MG/src/main/frontend/src/axiosSetup.js
+++ MG/src/main/frontend/src/axiosSetup.js
@@ -13,9 +13,11 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    if (token) { config.headers.Authorization = `Bearer ${token}`; }
+    if (token && !config.url.includes("/login") && !config.url.includes("/user")) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
-  },
+  },  
   (error) => { return Promise.reject(error); }
 );
 
